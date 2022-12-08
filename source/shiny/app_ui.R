700a4b9913174310936a4997cbb81f46eb351b10
samiiray <sy12345@uw.edu>
2022-12-07 18:55:45 -0800

    intro

--- source/shiny/app_ui.R
+++ source/shiny/app_ui.R
@@ -2,16 +2,38 @@
 happiness <- read.csv("https://raw.githubusercontent.com/info201b-au2022/happiness-levels/main/data/2019_happiness.csv")
 countries <- as.list(happiness$Country.or.region)
 
-intro_main_content <- mainPanel()
+intro_main_content <- mainPanel(
+h1("Introduction"),
 
-intro_panel <- tabPanel(
-  "Introduction",
-  
-  titlePanel("Introduction"),
-  
-  intro_main_content
+p(" Individual moods and conduct are significantly impacted by happiness. 
+Even more crucial is knowing what influences our happiness. We looked 
+at this issue from a wider perspective in an effort to comprehend this 
+query. Our analysis will center on the Gallup World Poll 2019 Report and 
+the associated factors. In order to identify the elements that are most 
+strongly associated with happiness, this initiative evaluated variables 
+like health, economic conditions, and freedom. We consider this subject 
+to be significant due to its connection to mental health. For this reason, 
+we're looking for a correlation between the data's variables and the 
+happiness index. By analyzing patterns, outliers, and elements like geography,
+we seek to better comprehend and illustrate what makes us happy."),
+
+h1("Our Dataset"),
+
+p("In this project, we have included seven variables. To gain a better 
+understanding of what determines and affects happiness, we chose freedom, 
+health, suicide rates, urbanization, fertility rates, GDP, and age. We 
+included three interactive charts in this project to teach our users about 
+how the variables we chose affect happiness globally.")
 )
 
+intro_panel <- tabPanel(
+   "Home",
+   
+titlePanel("Home"),
+  
+intro_main_content
+
+)
 # chart 1
 
 chart1_sidebar_content <- sidebarPanel(
